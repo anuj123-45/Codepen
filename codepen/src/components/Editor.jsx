7ef4877ca9e8880c0d9b14d4b6afc9702d885cda
anuj123-45 <anuj1358.be20@chitkara.edu.in>
2023-02-22 09:12:52 +0530

    changes done

--- codepen/src/components/Editor.jsx
+++ codepen/src/components/Editor.jsx
@@ -1,62 +1,61 @@
-import React from 'react';
-import {Box,styled} from '@mui/material';
-import CloseFullscreenIcon from '@mui/icons-material/CloseFullscreen';
-import {Controlled as CodeMirror} from 'react-codemirror2';
-import 'codemirror/lib/codemirror.css';
-import 'codemirror/theme/material.css';
-import 'codemirror/mode/xml/xml';
-import 'codemirror/mode/javascript/javascript';
-import 'codemirror/mode/css/css';
-
+import React from "react";
+import { Box, styled } from "@mui/material";
+import '../App.css';
+import CloseFullscreenIcon from "@mui/icons-material/CloseFullscreen";
+import { Controlled as CodeMirror } from "react-codemirror2";
+import "codemirror/lib/codemirror.css";
+import "codemirror/theme/material.css";
+import "codemirror/mode/xml/xml";
+import "codemirror/mode/javascript/javascript";
+import "codemirror/mode/css/css";
 
 export default function Editor() {
-  const Heading=styled(Box)`
+  const Heading = styled(Box)`
     background: #1d1e22;
     display: flex;
     padding: 9px 12px;
-  
+  `;
 
-    
-    `
-
-const Header=styled(Box)`
-display:flex;
-background:#060606;
-color:#AAAEBC;
-justify-content:space-between;
-font-weight:700px;
-
-
-`
+  const Header = styled(Box)`
+    display: flex;
+    background: #060606;
+    color: #aaaebc;
+    justify-content: space-between;
+    font-weight: 700px;
+  `;
 
   return (
     <Box>
-<Header>
-<Heading>
-<Box 
-component='span'
-style={{
-  background:'red',
-  height:20,
-  width:30,
-  display:'flex',
-  placeContent:'center',
-  borderRadius:5,
-  marginRight:9,
-  padding:2,
+      <Header>
+        <Heading>
+          <Box
+            component="span"
+            style={{
+              background: "red",
+              height: 20,
+              width: 30,
+              display: "flex",
+              placeContent: "center",
+              borderRadius: 5,
+              marginRight: 9,
+              padding: 2,
+            }}
+          >
+            /
+          </Box>
+          HTML
+        </Heading>
+        <CloseFullscreenIcon />
+      </Header>
 
-}}
-
-
->
-/
-</Box>
-HTML
-</Heading>
-<CloseFullscreenIcon/>
-
-</Header>
-<CodeMirror/>
+      <CodeMirror 
+      className="contolled-editor"
+      options={{
+        mode: 'xml',
+        theme: 'material',
+        lineNumbers: true
+      }}
+      />
     </Box>
-  )
+  );
 }
